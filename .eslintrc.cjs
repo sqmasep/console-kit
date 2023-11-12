01e2135bd39621320241030847c0a3053c600aef
sqmasep <leblond.romain@orange.fr>
2023-11-12 14:51:29 +0100

    [CONFIG] ESLint config, gitignore, prettier config, tsconfig & prepared package.json

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  env: { browser: true, es2020: true, node: true },
+  extends: ["eskiu/ts"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [],
+  rules: {},
+};
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+const config = {
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  arrowParens: "avoid",
+};
+
+module.exports = config;
