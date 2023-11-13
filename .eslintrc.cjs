c8a39d3b873c7c40fc508ea1c7526a9ced496627
sqmasep <leblond.romain@orange.fr>
2023-11-13 21:14:35 +0100

    [CONFIG] Changed eslint config and added ts-node & node types

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,5 +8,52 @@ module.exports = {
     sourceType: "module",
   },
   plugins: [],
-  rules: {},
+  rules: {
+    // same as eslint-config-eskiu/ts, but without the boolean prefixes rule
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "variable",
+        modifiers: ["destructured"],
+        format: null,
+      },
+      {
+        selector: "typeParameter",
+        format: ["PascalCase"],
+        prefix: ["T"],
+        leadingUnderscore: "forbid",
+        trailingUnderscore: "forbid",
+      },
+      {
+        selector: "interface",
+        format: ["PascalCase"],
+        leadingUnderscore: "forbid",
+        trailingUnderscore: "forbid",
+        custom: {
+          regex: "^I[A-Z]",
+          match: false,
+        },
+      },
+      {
+        selector: "typeLike",
+        format: ["PascalCase"],
+        leadingUnderscore: "forbid",
+        trailingUnderscore: "forbid",
+      },
+      {
+        selector: "memberLike",
+        modifiers: ["private"],
+        format: ["camelCase"],
+        leadingUnderscore: "require",
+      },
+    ],
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector: "TSEnumDeclaration",
+        message:
+          "Enums are not allowed. Consider using an object or a map instead.",
+      },
+    ],
+  },
 };
